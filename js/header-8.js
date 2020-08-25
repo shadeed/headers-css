85923be71c646276c37f085aeb88377771a0a957
Ahmad Shadeed <shadeed9@gmail.com>
2020-08-25 11:47:36 +0300

    Worked on three more headers

---
+++ js/header-8.js
@@ -0,0 +1,24 @@
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+let searchToggle = document.querySelector(".search__toggle");
+let searchForm = document.querySelector(".search__form");
+
+navToggle.addEventListener("click", showNav);
+
+searchToggle.addEventListener("click", showSearch);
+
+function showNav() {
+  navWrapper.classList.toggle("active");
+}
+
+function showSearch() {
+  searchForm.classList.toggle("active");
+  searchToggle.classList.toggle("active");
+
+  if (searchToggle.classList.contains("active")) {
+    searchToggle.setAttribute("aria-label", "Close search");
+  } else {
+    searchToggle.setAttribute("aria-label", "Open search");
+  }
+}
